399fb50ddc722b463994862655c10906f63e2255
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-11 17:13:45 -0600

    Update User.cpp

---
+++ User.cpp
@@ -0,0 +1,7 @@
+#include "user.h"
+
+// Constructor 
+User::User(int age, const std::string& gender, bool pre_existing_cond, bool vaccines)
+    : age(age), gender(gender), pre_existing_cond(pre_existing_cond), vaccines(vaccines) {}
+
+
