e8cdc9412171e9b822b3567e5e94b1764d72dcd2
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-11 17:12:56 -0600

    Update RiskFactor.cpp

---
+++ RiskFactor.cpp
@@ -0,0 +1,4 @@
+#include "RiskFactor.h"
+//Constructor 
+RiskFactor::RiskFactor(const std::string& climate, int population, int hygene)
+    : climate(climate), population(population), hygene(hygene) {}
