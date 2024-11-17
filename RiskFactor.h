ec5a61ad63402d7635c5132545ff049e7957c134
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-17 14:21:20 -0600

    Update RiskFactor.h

--- RiskFactor.h
+++ RiskFactor.h
@@ -1,12 +1,26 @@
+#ifndef RISKFACTOR_H
+#define RISKFACTOR_H
+
 #include <string>
+
 class RiskFactor{
     private: 
         std::string climate;
         int population;
-        int hygene;
+        int hygene_level;
     public: 
         // Constructor 
-        RiskFactor(const std::string& climate, int population, int hygene)
-            :climate(climate), population(population), hygene(hygene){}
-            
+        RiskFactor(const std::string& climate, int population, int hygene);
+        
+        //Setters  
+        void setClimate(const std::string& climate); 
+        void setPopulation(int population);
+        void setHygene_Level(int hygene);
+
+        //Getters 
+        std::string getClimate() const;
+        int getPopulation() const;
+        int getHygiene_Level() const;
 }; 
+
+#endif
