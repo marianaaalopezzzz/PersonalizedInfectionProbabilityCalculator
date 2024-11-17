7464ecb7cc0ba4faec77572d5692234cb5bcff2e
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-17 15:38:23 -0600

    Update RiskFactor.cpp

--- RiskFactor.cpp
+++ RiskFactor.cpp
@@ -1,4 +1,15 @@
-#include "RiskFactor.h"
-//Constructor 
-RiskFactor::RiskFactor(const std::string& climate, int population, int hygene)
-    : climate(climate), population(population), hygene(hygene) {}
+#include "RiskCalculator.h"
+//Constructor
+RiskCalculator::RiskCalculator(const User& use, const Disease& Disease, const RiskFactor& riskFactor)
+    : user(use), disease(Disease), riskfactor(riskFactor), base_risk(0.0){}
+
+// Setters
+void RiskCalculator::setBaseRisk(double risk) { this->base_risk = risk; }
+
+// Getters
+double RiskCalculator::getBaseRisk() const { return base_risk; }
+
+// Calculate Risk
+double RiskCalculator::calculateRisk() {
+    return base_risk; 
+}
