7c9be644fb05866cff3258ffe3e6bf49c85e91ba
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-17 15:37:41 -0600

    Update RiskCalculator.cpp

--- RiskCalculator.cpp
+++ RiskCalculator.cpp
@@ -1,6 +1,15 @@
 #include "RiskCalculator.h"
 //Constructor
-RiskCalculator::RiskCalculator(const User& use, const Disease& dise, const RiskFactor& riskfact)
-    : user(use), disease(dise), riskfactor(riskfact), base_risk(0.0){}
-void RiskCalculator::start(){}
-void RiskCalculator::stop() {}
+RiskCalculator::RiskCalculator(const User& use, const Disease& Disease, const RiskFactor& riskFactor)
+    : user(use), disease(Disease), riskfactor(riskFactor), base_risk(0.0){}
+
+// Setters
+void RiskCalculator::setBaseRisk(double risk) { this->base_risk = risk; }
+
+// Getters
+double RiskCalculator::getBaseRisk() const { return base_risk; }
+
+// Calculate Risk
+double RiskCalculator::calculateRisk() {
+    return base_risk; 
+}
