d6a5eedc47dba5a7f07c144b5f33d954eb2d5315
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-23 15:33:04 -0600

    Update Bacteria.cpp

--- Bacteria.cpp
+++ Bacteria.cpp
@@ -1,4 +1,8 @@
 #include "Bacteria.h"
 //Constructor 
-Bacteria::Bacteria(const std::string& name, double transmission_prob, int severity, const std::string& transmission_method, bool antibiotic_resistance)
+Bacteria::Bacteria(std::string name, double transmission_prob, int severity, std::string transmission_method, bool antibiotic_resistance)
     :Disease(name, transmission_prob, severity, transmission_method), antibiotic_resistance(antibiotic_resistance){}
+
+bool Bacteria::hasAntibioticResistance() {
+    return antibiotic_resistance;
+}
