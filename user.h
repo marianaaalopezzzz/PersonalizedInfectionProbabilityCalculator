6f46a78b8e7784eb1993d78ffe9a13647336b3a7
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-23 15:28:20 -0600

    Update user.h

--- user.h
+++ user.h
@@ -11,20 +11,19 @@ class User{
         bool vaccines;
     public: 
         // Constructor 
-         User(int age, const std::string& gender, bool pre_existing_cond, bool vaccines);
+         User(int age, std::string gender, bool pre_existing_cond, bool vaccines);
         
         // Setters 
         void setAge(int age); 
-        void setGender(const std::string& gender);
+        void setGender(std::string gender);
         void setPre_Existing_Cond(bool condition);
         void setVaccines(bool vaccinated);
 
         //Getters 
-        int getAge() const;
-        std::string getGender() const; 
-        bool hasPre_Existing_Cond() const; 
-        bool hasVaccines() const; 
+        int getAge();
+        std::string getGender(); 
+        bool hasPre_Existing_Cond(); 
+        bool hasVaccines(); 
 
 };
 #endif
-
