5b1ca87946a0ba0c2e9aec5a763c6181e49dd6cc
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-23 15:30:21 -0600

    Update Virus.cpp

--- Virus.cpp
+++ Virus.cpp
@@ -1,5 +1,8 @@
 #include "Virus.h"
 //Constructor 
-Virus::Virus(const std::string& name, double transmission_prob, int severity, const std::string& transmission_method, double mutation)
+Virus::Virus(std::string name, double transmission_prob, int severity, std::string transmission_method, double mutation)
     :Disease(name, transmission_prob, severity, transmission_method), mutation(mutation) {}
-    
+
+double Virus::getMutationRate(){
+    return mutation; 
+}
