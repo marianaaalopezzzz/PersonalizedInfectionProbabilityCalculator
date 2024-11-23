b453c3d73b63e0188a3b358806cc97d1ad2074fe
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-23 15:31:17 -0600

    Update RiskFactor.cpp

--- RiskFactor.cpp
+++ RiskFactor.cpp
@@ -1,15 +1,15 @@
 #include "RiskFactor.h"
 //Constructor 
-RiskFactor::RiskFactor(const std::string& climate, int population, int hygene)
-    : climate(climate), population(population), hygene_level(hygene) {}
+RiskFactor::RiskFactor(std::string climate, int population, int hygiene)
+    : climate(climate), population(population), hygiene_level(hygiene) {}
 
 //Setters 
-void RiskFactor::setClimate(const std::string& climate) { this->climate = climate; }
+void RiskFactor::setClimate(std::string climate) { this->climate = climate; }
 void RiskFactor::setPopulation(int population) { this->population = population; }
-void RiskFactor::setHygene_Level(int hygene) { this->hygene_level = hygene; }
+void RiskFactor::setHygiene_Level(int hygiene) { this->hygiene_level = hygiene; }
 
 // Getters
-std::string RiskFactor::getClimate() const { return climate; }
-int RiskFactor::getPopulation() const { return population; }
-int RiskFactor::getHygiene_Level() const { return hygene_level; }
+std::string RiskFactor::getClimate() { return climate; }
+int RiskFactor::getPopulation() { return population; }
+int RiskFactor::getHygiene_Level() { return hygiene_level; }
 
