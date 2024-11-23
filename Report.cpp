71891fcec44241494f96edd7ff954db455ef2c6a
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-23 15:32:16 -0600

    Update Report.cpp

--- Report.cpp
+++ Report.cpp
@@ -1,14 +1,14 @@
 #include "Report.h"
 //Constructor 
-Report::Report(const User& user, const Disease& disease, const RiskFactor& riskFactor, double probability, const std::string& summary, const std::string& recommendations)
-    :RiskCalculator(user,disease, riskFactor), final_calculated_probability(probability), summary(summary), recommendations(recommendations) {}
+Report::Report(User user, Disease disease, RiskFactor riskFactor, double probability, std::string summary, std::string recommendations)
+    :riskCalculator(user,disease, riskFactor), final_calculated_probability(probability), summary(summary), recommendations(recommendations) {}
 
 // Setters
 void Report::setFinal_Calculated_Probability(double final_calculated_probability) { this->final_calculated_probability = final_calculated_probability; }
-void Report::setSummary(const std::string& summary) { this->summary = summary; }
-void Report::setRecommendations(const std::string& recommendations) { this->recommendations = recommendations; }
+void Report::setSummary(std::string summary) { this->summary = summary; }
+void Report::setRecommendations(std::string recommendations) { this->recommendations = recommendations; }
 
 // Getters
-double Report::getFinal_Calculated_Probability() const { return final_calculated_probability; }
-std::string Report::getSummary() const { return summary; }
-std::string Report::getRecommendations() const { return recommendations; }
+double Report::getFinal_Calculated_Probability() { return final_calculated_probability; }
+std::string Report::getSummary() { return summary; }
+std::string Report::getRecommendations() { return recommendations; }
