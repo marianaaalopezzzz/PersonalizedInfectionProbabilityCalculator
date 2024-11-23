bd6a617d3286df859e55a2271770bbf848c3c5c4
Mariana López Colín <lopez.mariana.co@gmail.com>
2024-11-23 15:30:40 -0600

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,17 +1,17 @@
 #include "user.h"
 
 // Constructor 
-User::User(int age, const std::string& gender, bool pre_existing_cond, bool vaccines)
+User::User(int age, std::string gender, bool pre_existing_cond, bool vaccines)
     : age(age), gender(gender), pre_existing_cond(pre_existing_cond), vaccines(vaccines) {}
 
 // Setters 
 void User::setAge(int age) {this -> age = age; }
-void User::setGender(const std::string& gender) {this -> gender= gender;}
+void User::setGender(std::string gender) {this -> gender= gender;}
 void User::setPre_Existing_Cond(bool condition) {this -> pre_existing_cond = condition; }
 void User::setVaccines(bool vaccinated) {this -> vaccines = vaccinated; }
 
 //Getters 
-int User::getAge() const {return age; }
-std::string User::getGender() const {return gender; }
-bool User::hasPre_Existing_Cond() const {return pre_existing_cond; }
-bool User::hasVaccines() const { return vaccines; }
+int User::getAge() {return age; }
+std::string User::getGender() {return gender; }
+bool User::hasPre_Existing_Cond() {return pre_existing_cond; }
+bool User::hasVaccines() { return vaccines; }
